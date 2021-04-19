274013d985e07559daaf754a2647f4c876d17b1a
yap-yap-yap <zhihanyap@gmail.com>
2021-04-19 11:03:23 +0800

    Update AuthUtils

--- ProgrammingAssignment2/src/main/java/AuthUtils/RSAKeyUtils.java
+++ ProgrammingAssignment2/src/main/java/AuthUtils/RSAKeyUtils.java
@@ -6,17 +6,17 @@ import java.security.*;
 import java.util.Base64;
 
 public class RSAKeyUtils {
-    public static byte[] encrypt_string(String string, Key key) throws Exception{
+    public static byte[] encrypt_bytes(byte[] bytes, Key key) throws Exception{
         Cipher encryptCipher = getCipher(Cipher.ENCRYPT_MODE, key);
-        byte[] encryptBytes = encryptCipher.doFinal(string.getBytes());
+        byte[] encryptBytes = encryptCipher.doFinal(bytes);
         return encryptBytes;
 
     }
 
-    public static String decrypt_string(byte[] bytes, Key key) throws Exception{
+    public static byte[] decrypt_bytes(byte[] bytes, Key key) throws Exception{
         Cipher decryptCipher = getCipher(Cipher.DECRYPT_MODE, key);
         byte[] decryptBytes = decryptCipher.doFinal(bytes);
-        return Base64.getEncoder().encodeToString(decryptBytes);
+        return decryptBytes;
 
     }
 
